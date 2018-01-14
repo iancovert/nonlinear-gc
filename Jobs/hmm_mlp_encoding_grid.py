fc667c6f7855b1d5cfa8d0299ed0777afc2f45c5
Ian Connick Covert <icovert@cs.washington.edu>
2018-01-14 15:47:18 -0800

    Standardized HMM experiments

--- Jobs/hmm_mlp_encoding_grid.py
+++ Jobs/hmm_mlp_encoding_grid.py
@@ -8,39 +8,40 @@ tstamp = time.strftime('%H%M%S')
 jobname = 'hmm_mlp_encoding_%s_%s' % (dstamp, tstamp)
 jobfile = 'Batches/%s.job' % jobname
 
-nepoch_grid = [20000]
-lr_grid = [0.01]
-lam_grid = np.append(np.geomspace(3.0, 0.001, num = 50), 0)
+lam_grid = np.append(np.geomspace(10.0, 0.001, num = 50), 0)
 seed_grid = [0]
-hidden_grid = [5, 10]
-network_lag_grid = [5, 10]
+hidden_grid = [10]
+network_lag_grid = [3]
+
+nepoch_grid = [5000]
 
 sparsity_grid = [0.3]
 p_grid = [10]
 T_grid = [500]
-states_grid = [5]
+states_grid = [3]
 
 BASECMD = 'python hmm_mlp_encoding.py'
 
-param_grid = product(nepoch_grid, lr_grid, lam_grid, seed_grid, hidden_grid, network_lag_grid,
+param_grid = product(lam_grid, seed_grid, hidden_grid, network_lag_grid,
+	nepoch_grid,
 	sparsity_grid, p_grid, T_grid, states_grid)
 
 with open(jobfile, 'w') as f:
 	for param in param_grid:
-		nepoch, lr, lam, seed, hidden, network_lag, sparsity, p, T, states = param
+		lam, seed, hidden, network_lag, nepoch, sparsity, p, T, states = param
 
 		argstr = BASECMD
-		argstr += ' --nepoch=%d' % nepoch
-		argstr += ' --lr=%e' % lr
+
 		argstr += ' --lam=%e' % lam
 		argstr += ' --seed=%d' % seed
 		argstr += ' --hidden=%d' % hidden
 		argstr += ' --network_lag=%d' % network_lag
 
+		argstr += ' --nepoch=%d' % nepoch
+		
 		argstr += ' --sparsity=%e' % sparsity
 		argstr += ' --p=%d' % p
 		argstr += ' --T=%d' % T
 		argstr += ' --states=%d' % states
 
 		f.write(argstr + '\n')
-		
