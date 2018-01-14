1cfc41a2c1d9529ed82b4b97c44c8eb9234d1950
Ian Connick Covert <icovert@cs.washington.edu>
2018-01-14 15:29:52 -0800

    Oscillator experiment for MLP

--- Experiments/oscillator_mlp_encoding.py
+++ Experiments/oscillator_mlp_encoding.py
@@ -9,39 +9,43 @@ import shutil
 import sys
 
 # Data modules
-from Data.generate_synthetic import long_lag_var_model
-from Data.data_processing import format_ts_data, normalize
+from Data.generate_synthetic import kuramoto_model
+from Data.data_processing import format_replicate_ts_data, normalize
 
 # Model modules
 sys.path.append('../Model')
 from mlp import ParallelMLPEncoding
-from experiment_cv import run_experiment
+from experiment_opt import run_experiment
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--nepoch', type = int, default = 1000, help = 'number of training epochs')
-parser.add_argument('--lr', type = float, default = 0.001, help = 'learning rate')
+
 parser.add_argument('--lam', type = float, default = 0.1, help = 'lambda for weight decay')
 parser.add_argument('--seed', type = int, default = 12345, help = 'seed')
 parser.add_argument('--hidden', type = int, default = 10, help = 'hidden units')
-parser.add_argument('--network_lag', type = int, default = 5, help = 'lag considered by MLP')
+parser.add_argument('--network_lag', type = int, default = 2, help = 'lag considered by MLP')
 
-parser.add_argument('--sparsity', type = float, default = 0.2, help = 'sparsity of time series')
+parser.add_argument('--nepoch', type = int, default = 1000, help = 'number of training epochs')
+parser.add_argument('--lr', type = float, default = 0.01, help = 'learning rate')
+parser.add_argument('--cooldown', type = str, default = 'Y', help = 'learning rate cooldown')
+
+parser.add_argument('--sparsity', type = float, default = 0.3, help = 'sparsity of time series')
 parser.add_argument('--p', type = int, default = 10, help = 'dimensionality of time series')
-parser.add_argument('--T', type = int, default = 1000, help = 'length of time series')
-parser.add_argument('--lag', type = int, default = 2, help = 'lag in VAR model')
+parser.add_argument('--T', type = int, default = 250, help = 'length of time series')
+parser.add_argument('--trials', type = int, default = 10, help = 'number of replicates')
 
 parser.add_argument('--loss_check', type = int, default = 10, help = 'interval for checking loss')
 
 args = parser.parse_args()
 
 # Prepare filename
-experiment_base = 'Long VAR MLP Encoding'
+experiment_base = 'Oscillator MLP Encoding'
 results_dir = 'Results/' + experiment_base
 
 experiment_name = results_dir + '/expt'
-experiment_name += '_nepoch=%d_lr=%e_lam=%e_seed=%d_hidden=%d_networklag=%d' % (args.nepoch, args.lr, args.lam, args.seed, args.hidden, args.network_lag)
-experiment_name += '_spars=%e_p=%d_T=%d_lag=%d.out' % (args.sparsity, args.p, args.T, args.lag) 
+experiment_name += '_nepoch=%d_lr=%e_cooldown=%s' % (args.nepoch, args.lr, args.cooldown)
+experiment_name += '_lam=%e_seed=%d_hidden=%d_networklag=%d' % (args.lam, args.seed, args.hidden, args.network_lag)
+experiment_name += '_spars=%e_p=%d_T=%d_trials=%d.out' % (args.sparsity, args.p, args.T, args.trials)
 
 # Create directory, if necessary
 if not os.path.exists(results_dir):
@@ -53,26 +57,24 @@ if os.path.isfile(experiment_name):
 	sys.exit(0)
 
 # Prepare data
-X, _, GC = long_lag_var_model(args.sparsity, args.p, 1.0, 1.0, args.T, lag = args.lag)
+X, GC = kuramoto_model(args.sparsity, args.p, num_trials = args.trials, N = args.T, standardized = True)
 X = normalize(X)
-X_train, Y_train, X_val, Y_val = format_ts_data(X, args.network_lag)
-
-p_in = Y_val.shape[1]
-p_out = Y_val.shape[1]
+X_train, Y_train, _, _ = format_replicate_ts_data(X, args.network_lag, validation = 0.0)
 
 # Get model
 if args.seed != 0:
 	torch.manual_seed(args.seed)
-model = ParallelMLPEncoding(p_in, p_out, args.network_lag, [args.hidden], args.lr, 'prox', args.lam, 'group_lasso')
+model = ParallelMLPEncoding(Y_train.shape[1], Y_train.shape[1], args.network_lag, [args.hidden], args.lr, 'prox', args.lam, 'group_lasso')
 
 # Run experiment
-train_loss, val_loss, best_properties = run_experiment(model, X_train, Y_train, X_val, Y_val, args.nepoch, predictions = True, loss_check = args.loss_check)
+train_loss, train_objective, best_properties = run_experiment(model, X_train, Y_train, args.nepoch, predictions = True, loss_check = args.loss_check, cooldown = args.cooldown.lower() == 'y')
 
 # Format results
 experiment_params = {
 	'nepoch': args.nepoch,
-	'lam': args.lam,
 	'lr': args.lr,
+	'cooldown': args.cooldown,
+	'lam': args.lam,
 	'seed': args.seed,
 	'hidden': args.hidden,
 	'network_lag': args.network_lag
@@ -82,15 +84,14 @@ data_params = {
 	'sparsity': args.sparsity,
 	'p': args.p,
 	'T': args.T,
-	'lag': args.lag,
+	'trials': args.trials,
 	'GC_true': GC
 }
 
 best_results = {
 	'best_nepoch': [props['nepoch'] for props in best_properties],
-	'best_val_loss': [props['val_loss'] for props in best_properties],
+	'best_objective': [props['train_objective'] for props in best_properties],
 	'predictions_train': np.concatenate([props['predictions_train'][:, np.newaxis] for props in best_properties], axis = 1),
-	'predictions_val': np.concatenate([props['predictions_val'][:, np.newaxis] for props in best_properties], axis = 1),
 	'GC_est': [np.linalg.norm(np.reshape(props['weights'], newshape = (args.hidden * args.network_lag, args.p), order = 'F'), axis = 0) for props in best_properties]
 }
 
--- Jobs/oscillator_mlp_encoding_grid.py
+++ Jobs/oscillator_mlp_encoding_grid.py
@@ -5,31 +5,30 @@ import time
 dstamp = time.strftime('%Y%m%d')
 tstamp = time.strftime('%H%M%S')
 
-jobname = 'dream_mlp_encoding_%s_%s' % (dstamp, tstamp)
+jobname = 'oscillator_mlp_encoding_%s_%s' % (dstamp, tstamp)
 jobfile = 'Batches/%s.job' % jobname
 
 lam_grid = np.append(np.geomspace(10.0, 0.001, num = 50), 0)
 seed_grid = [0]
-hidden_grid = [4, 8]
+hidden_grid = [10]
 network_lag_grid = [2]
 
 nepoch_grid = [5000]
-lr_grid = [0.01]
-cooldown_grid = ['Y']
 
-size_grid = [50]
-type_grid = ['Ecoli', 'Yeast']
-number_grid = [1, 2, 3]
+sparsity_grid = [0.3]
+p_grid = [10]
+T_grid = [250]
+trials_grid = [10]
 
-BASECMD = 'python dream_mlp_encoding.py'
+BASECMD = 'python oscillator_mlp_encoding.py'
 
 param_grid = product(lam_grid, seed_grid, hidden_grid, network_lag_grid,
-	nepoch_grid, lr_grid, cooldown_grid,
-	size_grid, type_grid, number_grid)
+	nepoch_grid,
+	sparsity_grid, p_grid, T_grid, trials_grid)
 
 with open(jobfile, 'w') as f:
 	for param in param_grid:
-		lam, seed, hidden, network_lag, nepoch, lr, cooldown, size, typ, number = param
+		lam, seed, hidden, network_lag, nepoch, sparsity, p, T, trials = param
 
 		argstr = BASECMD
 
@@ -39,11 +38,10 @@ with open(jobfile, 'w') as f:
 		argstr += ' --network_lag=%d' % network_lag
 
 		argstr += ' --nepoch=%d' % nepoch
-		argstr += ' --lr=%e' % lr
-		argstr += ' --cooldown=%s' % cooldown
 		
-		argstr += ' --size=%d' % size
-		argstr += ' --type=%s' % typ
-		argstr += ' --number=%d' % number
+		argstr += ' --sparsity=%e' % sparsity
+		argstr += ' --p=%d' % p
+		argstr += ' --T=%d' % T
+		argstr += ' --trials=%d' % trials
 
 		f.write(argstr + '\n')
