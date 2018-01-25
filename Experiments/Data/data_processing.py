Removed momentum for prox gradient descent. Removed global rescaling of data. Fixed bug related to saving best weights

Experiments/Data/data_processing.py
@@ -90,7 +90,7 @@ def whiten_data_cholesky(X):
 
 	return np.dot(X_centered, L_inv.T)
 
-def normalize(X, scale_global = True):
+def normalize(X, scale_global = False):
 	if len(X.shape) == 2:
 		X_centered = X - np.mean(X, axis = 0)
 		

Model/lstm.py
@@ -31,7 +31,7 @@ class ParallelLSTMEncoding:
 		self.opt = opt
 
 		if opt == 'prox':
-			self.optimizers = [optim.SGD(list(lstm.parameters()) + list(out.parameters()), lr = lr, momentum = 0.9) for lstm, out in zip(self.lstms, self.out_layers)]
+			self.optimizers = [optim.SGD(list(lstm.parameters()) + list(out.parameters()), lr = lr, momentum = 0.0) for lstm, out in zip(self.lstms, self.out_layers)]
 			self.train = self._train_prox
 
 		else:
@@ -48,7 +48,7 @@ class ParallelLSTMEncoding:
 
 	def cooldown(self, p):
 		self.lr[p] *= self.lr_decay
-		if self.opt == 'prox' or self.opt == 'momentum':
+		if self.opt == 'momentum':
 			self.optimizers[p] = optim.SGD(list(self.lstms[p].parameters()) + list(self.out_layers[p].parameters()), lr = self.lr[p], momentum = 0.9)
 		elif self.opt == 'adam':
 			self.optimizers[p] = optim.Adam(list(self.lstms[p].parameters()) + list(self.out_layers[p].parameters()), lr = self.lr[p])
@@ -150,7 +150,7 @@ class ParallelLSTMEncoding:
 		[optimizer.step() for optimizer in self.optimizers]
 
 		# Apply proximal operator
-		[prox_operator(lstm.weight_ih_l0, 'group_lasso', self.input_series, self.lr, self.lam) for lstm in self.lstms]
+		[prox_operator(lstm.weight_ih_l0, 'group_lasso', self.input_series, lr, self.lam) for (lstm, lr) in zip(self.lstms, self.lr)]
 
 		if return_hidden:
 			return hidden
@@ -181,9 +181,9 @@ class ParallelLSTMEncoding:
 
 	def get_weights(self, p = None):
 		if p is None:
-			return [lstm.weight_ih_l0.data.numpy() for lstm in self.lstms]
+			return [lstm.weight_ih_l0.data.numpy().copy() for lstm in self.lstms]
 		else:
-			return self.lstms[p].weight_ih_l0.data.numpy()
+			return self.lstms[p].weight_ih_l0.data.numpy().copy()
 
 class ParallelLSTMDecoding:
 	def __init__(self, input_series, output_series, hidden_size, hidden_layers, fc_units, lr, opt, lam, nonlinearity = 'relu'):
@@ -221,7 +221,7 @@ class ParallelLSTMDecoding:
 			param_list = param_list + list(net.parameters())
 
 		if opt == 'prox':
-			self.optimizer = optim.SGD(param_list, lr = lr, momentum = 0.9)
+			self.optimizer = optim.SGD(param_list, lr = lr, momentum = 0.0)
 			self.train = self._train_prox
 		else:
 			if opt == 'adam':
@@ -346,8 +346,11 @@ class ParallelLSTMDecoding:
 		objective = self._objective(X, Y)
 		return np.array([num.data[0] for num in objective])
 
-	def get_weights(self):
-		return [list(net.parameters())[0].data.numpy() for net in self.out_networks]
+	def get_weights(self, p = None):
+		if p is None:
+			return [list(net.parameters())[0].data.numpy().copy() for net in self.out_networks]
+		else:
+			return list(self.out_networks[p].parameters())[0].data.numpy().copy()
 
 
 def repackage_hidden(hidden):

Model/mlp.py
@@ -43,7 +43,7 @@ class ParallelMLPEncoding:
 		self.opt = opt
 
 		if opt == 'prox':
-			self.optimizers = [optim.SGD(list(net.parameters()), lr = lr, momentum = 0.9) for net in self.sequentials]
+			self.optimizers = [optim.SGD(list(net.parameters()), lr = lr, momentum = 0.0) for net in self.sequentials]
 			self.train = self._train_prox
 
 		else:
@@ -60,7 +60,7 @@ class ParallelMLPEncoding:
 
 	def cooldown(self, p):
 		self.lr[p] *= self.lr_decay
-		if self.opt == 'prox' or self.opt == 'momentum':
+		if self.opt == 'momentum':
 			self.optimizers[p] = optim.SGD(list(self.sequentials[p].parameters()), lr = self.lr[p], momentum = 0.9)
 		elif self.opt == 'adam':
 			self.optimizers[p] = optim.Adam(list(self.sequentials[p].parameters()), lr = self.lr[p])
@@ -79,7 +79,7 @@ class ParallelMLPEncoding:
 		[optimizer.step() for optimizer in self.optimizers]
 
 		# Apply prox operator
-		[prox_operator(list(net.parameters())[0], self.penalty, self.n, self.lr, self.lam, lag = self.lag) for net in self.sequentials]
+		[prox_operator(list(net.parameters())[0], self.penalty, self.n, lr, self.lam, lag = self.lag) for (net, lr) in zip(self.sequentials, self.lr)]
 
 	def _train_builtin(self, X, Y):
 		# Compute objective
@@ -115,9 +115,9 @@ class ParallelMLPEncoding:
 
 	def get_weights(self, p = None):
 		if p is None:
-			return [list(net.parameters())[0].data.numpy() for net in self.sequentials]
+			return [list(net.parameters())[0].data.numpy().copy() for net in self.sequentials]
 		else:
-			return list(self.sequentials[p].parameters())[0].data.numpy()
+			return list(self.sequentials[p].parameters())[0].data.numpy().copy()
 
 	def predict(self, X):
 		Y_pred = self._forward(X)
@@ -179,7 +179,7 @@ class ParallelMLPDecoding:
 			param_list = param_list + list(net.parameters())
 		
 		if opt == 'prox':
-			self.optimizer = optim.SGD(param_list, lr = lr, momentum = 0.9)
+			self.optimizer = optim.SGD(param_list, lr = lr, momentum = 0.0)
 			self.train = self._train_prox
 
 		else:
@@ -241,8 +241,11 @@ class ParallelMLPDecoding:
 		objective = self._objective(X, Y)
 		return np.array([num.data[0] for num in objective])
 
-	def get_weights(self):
-		return [list(net.parameters())[0].data.numpy() for net in self.out_nets]
+	def get_weights(self, p = None):
+		if p is None:
+			return [list(net.parameters())[0].data.numpy().copy() for net in self.out_nets]
+		else:
+			return list(self.out_nets[p].parameters())[0].data.numpy().copy()
 
 	def predict(self, X):
 		Y_pred = self._forward(X)

Model/regularize.py
@@ -14,33 +14,27 @@ def apply_penalty(W, penalty, num_inputs, lag = 1):
 
 def prox_operator(W, penalty, num_inputs, lr, lam, lag = 1):
 	if penalty == 'group_lasso':
-		_prox_group_lasso(W, penalty, num_inputs, lag, lr, lam)
+		_prox_group_lasso(W, num_inputs, lag, lr, lam)
+		# _prox_group_lasso_inplace(W, num_inputs, lag, lr, lam)
 	elif penalty == 'hierarchical':
-		_prox_hierarchical(W, penalty, num_inputs, lag, lr, lam)
+		_prox_hierarchical(W, num_inputs, lag, lr, lam)
 	else:
 		raise ValueError('penalty must be group_lasso or hierarchical')
 
-def _prox_group_lasso(W, penalty, num_inputs, lag, lr, lam):
+def _prox_group_lasso(W, num_inputs, lag, lr, lam):
 	'''
 		Apply prox operator directly (not through prox of conjugate)
 	'''
 	C = W.data.clone().numpy()
-
-	# if retain is not None:
-	# 	col = C[(retain + 1) * lag - 1, :].copy()
-	# 	C[(retain + 1) * lag - 1, :] = 0.0
 
 	h, l = C.shape
 	C = np.reshape(C, newshape = (lag * h, num_inputs), order = 'F')
 	C = _prox_update(C, lam, lr)
 	C = np.reshape(C, newshape = (h, l), order = 'F')
 
-	# if retain is not None:
-	# 	C[(retain + 1) * lag - 1, :] = col
-
 	W.data = torch.from_numpy(C)
 
-def _prox_hierarchical(W, penalty, num_inputs, lag, lr, lam):
+def _prox_hierarchical(W, num_inputs, lag, lr, lam):
 	''' 
 		Apply prox operator for each penalty
 	'''
@@ -53,14 +47,6 @@ def _prox_hierarchical(W, penalty, num_inputs, lag, lr, lam):
 		temp = C[range(end), :]
 		C[range(end), :] = _prox_update(temp, lam, lr)
 
-		# if i <= lag or retain is None:
-		# 	temp = C[range(end), :]
-		# 	C[range(end), :] = _prox_update(temp, lam, lr)
-		# else:
-		# 	inds = np.concatenate((np.arange(retain - 1), np.arange(retain + 1, p)))
-		# 	temp = C[range(end), inds]
-		# 	C[range(end), inds] = _prox_update(temp, lam, lr[inds])
-
 	C = np.reshape(C, newshape = (h, l), order = 'F')
 
 	W.data = torch.from_numpy(C)
@@ -69,8 +55,6 @@ def _prox_update(W, lam, lr):
 	'''
 		Apply prox operator to a matrix, where columns each have group lasso penalty
 	'''
-	if type(lr) is not float:
-		lr = np.array(lr)
 
 	W = W.copy()
 
@@ -78,6 +62,31 @@ def _prox_update(W, lam, lr):
 	norm_value_gt = norm_value >= (lam * lr)
 	
 	W[:, np.logical_not(norm_value_gt)] = 0.0
-	W[:, norm_value_gt] = W[:, norm_value_gt] * (1 - lam * np.divide(lr[norm_value_gt], norm_value[norm_value_gt][np.newaxis, :]))
+	W[:, norm_value_gt] = W[:, norm_value_gt] * (1 - np.divide(lam * lr, norm_value[norm_value_gt][np.newaxis, :]))
 
 	return W
+
+# def _prox_group_lasso_inplace(W, num_inputs, lag, lr, lam):
+# 	# TODO PyTorch complains that this does an in place edit of a Variable that requires gradient
+# 	for i in range(num_inputs):
+# 		norm = W[:, (lag * i):(lag * (i + 1))].norm()
+# 		if norm.data.numpy()[0] <= lr * lag:
+# 			W[:, (lag * i):(lag * (i + 1))] = 0.0
+# 		else:
+# 			W[:, (lag * i):(lag * (i + 1))] = W[:, (lag * i):(lag * (i + 1))] * (1 - lr * lam / norm)
+
+# Slower, secondary implementation of hierarchical update
+# def hierarchical_update(W, num_inputs, lag, lr, lam):
+# 	C = W.data.clone().numpy()
+
+# 	for i in range(num_inputs):
+# 		start = i * lag
+# 		for j in range(1, lag + 1):
+# 			norm = np.linalg.norm(C[:, range(start, start + j)], order = 2)
+# 			if norm < lam * lr:
+# 				C[:, range(start, start + j)] = 0.0
+# 			else:
+# 				C[:, range(start, start + j)] = C[:, range(start, start + j)] * (1 - lam * lr / norm)
+
+# 	W.data = torch.from_numpy(C)
+