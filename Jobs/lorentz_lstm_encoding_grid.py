420649b7a90f1a20dbf33236b93b546775057043
Ian Connick Covert <icovert@cs.washington.edu>
2018-02-06 01:29:00 -0800

    Setting up simulation study on Lorentz data

--- Jobs/lorentz_lstm_encoding_grid.py
+++ Jobs/lorentz_lstm_encoding_grid.py
@@ -19,20 +19,20 @@ wd_grid = [0.01]
 FC_grid = [10, 40]
 sd_grid = [2.0]
 dt_grid = [0.05]
-p_grid = [20]
-T_grid = [250, 500, 750, 1000, 1250, 1500]
-data_seed_grid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+p_grid = [10, 20, 30]
+T_grid = [500, 750, 1000]
+data_seed_grid = [0, 1, 2, 3, 4]
 
 BASECMD = 'python lorentz_lstm_encoding.py'
 
-param_grid = product(T_grid, data_seed_grid,
+param_grid = product(p_grid, T_grid, data_seed_grid,
 	lam_grid, seed_grid, hidden_grid,
 	nepoch_grid, lr_grid,
-	FC_grid, sd_grid, dt_grid, p_grid)
+	FC_grid, sd_grid, dt_grid)
 
 with open(jobfile, 'w') as f:
 	for param in param_grid:
-		T, data_seed, lam, seed, hidden, nepoch, lr, FC, sd, dt, p = param
+		p, T, data_seed, lam, seed, hidden, nepoch, lr, FC, sd, dt = param
 
 		argstr = BASECMD
 
--- Jobs/lorentz_mlp_encoding_grid.py
+++ Jobs/lorentz_mlp_encoding_grid.py
@@ -20,18 +20,19 @@ wd_grid = [0.01]
 FC_grid = [10, 40]
 sd_grid = [2.0]
 dt_grid = [0.05]
-p_grid = [20]
+p_grid = [10, 20, 30]
 T_grid = [500, 750, 1000]
+data_seed_grid = [0, 1, 2, 3, 4]
 
 BASECMD = 'python lorentz_mlp_encoding.py'
 
 param_grid = product(lam_grid, seed_grid, hidden_grid, network_lag_grid,
 	nepoch_grid, lr_grid, wd_grid,
-	FC_grid, sd_grid, dt_grid, p_grid, T_grid)
+	FC_grid, sd_grid, dt_grid, p_grid, T_grid, data_seed_grid)
 
 with open(jobfile, 'w') as f:
 	for param in param_grid:
-		lam, seed, hidden, network_lag, nepoch, lr, wd, FC, sd, dt, p, T = param
+		lam, seed, hidden, network_lag, nepoch, lr, wd, FC, sd, dt, p, T, data_seed = param
 
 		argstr = BASECMD
 
@@ -43,12 +44,12 @@ with open(jobfile, 'w') as f:
 		argstr += ' --nepoch=%d' % nepoch
 		argstr += ' --lr=%e' % lr
 		argstr += ' --weight_decay=%e' % wd
-		
+
 		argstr += ' --FC=%e' % FC
 		argstr += ' --sd=%e' % sd
 		argstr += ' --dt=%e' % dt
 		argstr += ' --p=%d' % p
 		argstr += ' --T=%d' % T
+		argstr += ' --data_seed=%d' % data_seed
 
 		f.write(argstr + '\n')
-
