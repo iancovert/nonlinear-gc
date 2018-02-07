6424eb0848d404d1c2be5cbe0d50db91bc770b84
Ian Covert <icc2115@columbia.edu>
2018-02-07 01:54:52 -0800

    Minor changes

--- Experiments/standardized_var_mlp_encoding.py
+++ Experiments/standardized_var_mlp_encoding.py
@@ -56,14 +56,14 @@ if os.path.isfile(experiment_name):
 	sys.exit(0)
 
 # Prepare data
-X, _, GC = standardized_var_model(args.sparsity, args.p, 5, 2.5, args.T, args.lag)
+X, _, GC = standardized_var_model(args.sparsity, args.p, 5, 0.5, args.T, args.lag)
 X = normalize(X)
 X_train, Y_train, _, _ = format_ts_data(X, args.network_lag, validation = 0.0)
 
 # Get model
 if args.seed != 0:
 	torch.manual_seed(args.seed)
-model = ParallelMLPEncoding(Y_train.shape[1], Y_train.shape[1], args.network_lag, [args.hidden], args.lr, 'line', args.lam, 'hierarchical')
+model = ParallelMLPEncoding(Y_train.shape[1], Y_train.shape[1], args.network_lag, [args.hidden], args.lr, 'line', args.lam, 'hierarchical', nonlinearity = 'sigmoid')
 
 # Run experiment
 train_loss, train_objective, weights, pred = run_experiment(model, X_train, Y_train, args.nepoch, predictions = True, loss_check = args.loss_check, verbose = True)
